9623e155e6ecd2eed42f399ed59ec1c541ce3119
apalii <apalii@alienvault.com>
2017-04-21 17:25:10 +0300

    Increasing JWT expiration to 1 hour

--- rest_proj/rest_proj/settings.py
+++ rest_proj/rest_proj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import datetime
 
 from decouple import config
 
@@ -42,7 +43,6 @@ if not DEBUG:
             'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
             'rest_framework.authentication.SessionAuthentication',
             'rest_framework.authentication.BasicAuthentication',
-            # 'rest_framework.authentication.TokenAuthentication',
         ),
         'DEFAULT_PERMISSION_CLASSES': (
             'rest_framework.permissions.IsAuthenticated',
@@ -51,6 +51,13 @@ if not DEBUG:
             'rest_framework.renderers.JSONRenderer',
         ),
     }
+
+# 60 seconds * minutes
+token_expiration = 60 * 60
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=token_expiration),
+}
 
 ALLOWED_HOSTS = [config('HOSTS')]
 
